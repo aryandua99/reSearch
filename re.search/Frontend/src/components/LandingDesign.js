b9c16fcb2ac65410b5722690a35fa4bbb57e3cb0
ibhattacharj <ibhattacharj@umass.edu>
2024-11-09 20:28:41 -0500

    changed color schemes of all pages and linked them

--- re.search/Frontend/src/components/LandingDesign.js
+++ re.search/Frontend/src/components/LandingDesign.js
@@ -1,25 +1,23 @@
-// src/components/LandingDesign.js
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './styling/LandingDesign.css' // Import CSS file for styling
-
-
-
+import './styling/LandingDesign.css'; // Import CSS file for styling
+import LoginForm from './LoginForm';
 
 function LandingDesign() {
    const navigate = useNavigate();
 
-
    return (
        <div className="landing-container">
-           <h1 className="landing-title">Welcome to Our Project</h1>
-           <div className="button-container">
-               <button className="landing-button" onClick={() => navigate('/signup')}>Sign Up</button>
-               <button className="landing-button" onClick={() => navigate('/login')}>Log In</button>
+           <div className="landing-content">
+               {/* <h1 className="landing-title">reSearch</h1> */}
+               <div className="button-container">
+                   {/* <button className="landing-button" onClick={() => navigate('/signup')}>Sign Up</button>
+                   <button className="landing-button" onClick={() => navigate('/login')}>Log In</button> */}
+                   <LoginForm />
+               </div>
            </div>
        </div>
    );
 }
-
 
 export default LandingDesign;
---
+++ re.search/Frontend/src/components/LoginForm.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+import './styling/RegistrationForm.css';
+
+
+import email_icon from './Assets/email.png';
+import user_icon from './Assets/user.png';
+import pwd_icon from './Assets/padlock.png';
+import cpwd_icon from './Assets/approve.png';
+import uni_icon from './Assets/university.png';
+
+
+
+
+const LoginForm = () => {
+    const navigate = useNavigate();
+
+    const handleSignUpRedirect = () => {
+       navigate('/signup');
+   };
+  
+   const handleSignUp = (event) => {
+       event.preventDefault();
+       const email = document.getElementById("email").value;
+       const pwd = document.getElementById("pwd").value;
+       const name = document.getElementById("name").value;
+       const cpwd = document.getElementById("cpwd").value;
+       // const uni = document.getElementById("universities").uni_id.value;
+
+
+       if (pwd !== cpwd) {
+           alert("Error: Confirmed password does not match password. Please re-enter your password.");
+           return; // Exit the function if passwords don't match
+       }
+
+
+       const user = {
+           email: email,
+           hashed_pwd: pwd, // Replace with actual hashed password
+           name: name,
+       };
+
+
+       console.log("User:", user);
+
+
+       // const createUser = async (user) => {
+       //     // console.log("In helper");
+       //     try {
+       //         const response = await axios.post('http://localhost:8080/api/v1/users/addUser', user);
+       //         // console.log(response.data);
+       //         // console.log("PASSED DATA");
+       //         navigate('/dashboard');
+       //     } catch (error) {
+       //         console.error(error);
+       //     }
+       // };
+
+
+       // createUser(user);
+   };
+
+
+   return (
+
+
+       <form>
+           {/* <div className='logo'>
+               <img src={logo2} alt=""/>
+           </div> */}
+           {/* In the CSS file, all of these inputs will be under the class name registration-form__input */}
+          
+           <div className='centering-container'>
+               <div className='reg_form_container'>
+                   <div className='header'>
+                       <div className='text'>Login</div>
+                       <div className='underline'></div>
+                   </div>
+                   <div className='fields'>
+
+
+                   <div className='field'>
+                       <img src={email_icon} alt=""/>
+                       <input type="email" id="email" name="email" placeholder='Email ID' autoComplete='off' /><br /><br />
+                   </div>
+
+
+                   <div className='field'>
+                       <img src={pwd_icon} alt=""/>
+                       <input type="password" id="pwd" name="pwd" placeholder='Password' autoComplete='off' /><br /><br />
+                   </div>
+                      
+                   {/* <div className='degree-level'>
+                       <img src={uni_icon} alt=""/>
+                       <input type="text" id="university" name="university" placeholder='University' autoComplete='off'/><br /><br />
+                   </div> */}
+                      
+
+
+               </div >
+                  
+                   <div className='submit-container'>
+                       <button type="submit" className='submit'>Log in</button>
+                       {/* <button type="submit" className='submit' onClick={handleSignUp}>Log in</button> */}
+                   </div>
+
+                   <p className='signup-redirect-text'>
+                       Don’t have an account? 
+                       <span 
+                           className="signup-link" 
+                           onClick={handleSignUpRedirect}
+                       >
+                           Sign up here
+                       </span>
+                   </p>
+
+
+                   </div>
+           </div>
+       </form>
+      
+   );
+};
+
+
+export default LoginForm;
